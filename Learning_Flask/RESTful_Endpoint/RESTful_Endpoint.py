cf21d6c027ed23ba71c74dac1a2031e0859ac9f6
MattiasV <mattias.vandecauter@gmail.com>
2021-09-28 13:47:04 +0200

    Update

---
+++ Learning_Flask/RESTful_Endpoint/RESTful_Endpoint.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", content_list=['idk',"aa","bb"])
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+if __name__ == "__main__":
+    app.run(debug=True)
