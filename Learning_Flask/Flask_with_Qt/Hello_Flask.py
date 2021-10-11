b418ec065d014de6929375733c4df74a145c2cda
MattiasV <mattias.vandecauter@gmail.com>
2021-10-11 13:39:45 +0200

    idk

--- Learning_Flask/Flask_with_Qt/Hello_Flask.py
+++ Learning_Flask/Flask_with_Qt/Hello_Flask.py
@@ -1,30 +1,13 @@
-import sys
-
-from PyQt5 import QtWidgets
 from flask import Flask
-from flask_restful import Resource, Api
-
-from qt_ui import Ui_Form
+from flask_restplus import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-
-    def get(self):
-        return {'hello': 'world', 'Hey': 'hoi'}
-
-class GUI_setup(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-
+@api.route('/adder/<int:a>/<int:b>')
+class SimpleAdder(Resource):
+    def get(self, a, b):
+        return {'calc': a + b}
 
 if __name__ == '__main__':
-    app_GUI = QtWidgets.QApplication(sys.argv)
-    w = GUI_setup()
-    w.show()
-    api.add_resource(HelloWorld, '/')
     app.run(debug=True)
